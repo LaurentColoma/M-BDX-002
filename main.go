da00064da35905dd69c05f88a4e83ef5687976bf
Laurent <coloma.laurent@gmail.com>
2021-02-15 20:33:51 +0100

    first line in progress

--- main.go
+++ main.go
@@ -1,22 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
-func main() {
+func NbArgsHandler() bool {
 	if len(os.Args) < 2 {
 		fmt.Println("Error: wrong number of arguments")
+		return false
+	}
+	return true
+}
+
+func OpenFileHandler(err error) bool {
+	if err != nil {
+		fmt.Println("Error: file not supported")
+		return false
+	}
+	return true
+}
+
+func FirstLineHandler(line string) bool {
+	return true
+}
+
+func ParsingHandler(line string, count int) bool {
+	if count == 0 {
+		return FirstLineHandler(line)
+	}
+	return true
+}
+
+func main() {
+	if NbArgsHandler() == false {
 		return
 	}
 
-	content, error := ioutil.ReadFile(os.Args[1])
+	content, err := os.Open(os.Args[1])
+	if OpenFileHandler(err) == false {
+		return
+	}
+	defer content.Close()
 
-	if error != nil {
-		fmt.Println("Error: file not supported")
+	count := 0
+	scanner := bufio.NewScanner(content)
+	for scanner.Scan() {
+		if ParsingHandler(scanner.Text(), count) == false {
+			return
+		}
 	}
 
-	fmt.Println(string(content))
+	return
 }
