fd65ead37384534d90e15483f6c10ccd82f179e9
Laurent <coloma.laurent@gmail.com>
2021-02-17 20:55:48 +0100

    Merge again

--- gameLoop/gameLoop.go
+++ gameLoop/gameLoop.go
@@ -6,17 +6,7 @@ import (
 	gameData "github.com/LaurentColoma/M-BDX-002/gameData"
 )
 
-<<<<<<< HEAD
 func gameLoop(warehouse gameData.Warehouse) {
-=======
-func gameLoop(warehouse gameData.Warehouse)  {
-	state := [...]string{"GO", "WAIT", "TAKE", "LEAVE", "GONE", "WAITING"}
-
-	gameData.Warehouse.Truck.Status = 5
-	for i := range warehouse.PalletTrucks {
-		gameData.Warehouse.PalletTrucks[i].Status = 1
-	}
->>>>>>> 7bc1be5ad53bd8d5998f3e33b9ac89dee8c2e13d
 	for i := 0; i < warehouse.NbTurn; i++ {
 		fmt.Printf("tour %v\n", i+1)
 
