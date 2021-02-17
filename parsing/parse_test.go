ed77aa20ecf43305044da1863f450894d46f6394
Laurent <coloma.laurent@gmail.com>
2021-02-18 00:22:28 +0100

    corrected parse_test.go

--- parsing/parse_test.go
+++ parsing/parse_test.go
@@ -2,11 +2,13 @@ package parsing
 
 import (
 	"testing"
+
+	parser "github.com/LaurentColoma/M-BDX-002/parsing"
 )
 
 func TestParsingHandler(t *testing.T) {
 
-	emptyResult := ParsingHandler("colis_a_livrer 2 1 green", 5, 0)
+	emptyResult := parser.ParsingHandler("colis_a_livrer 2 1 green", 5, 0)
 
 	if emptyResult != true {
 		t.Errorf("ParsingHandler(\"colis_a_livrer 2 1 green\") failed, expected true, got false")
